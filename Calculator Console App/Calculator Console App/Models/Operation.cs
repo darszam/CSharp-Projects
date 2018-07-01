d528531b2f7822781e5c43b2d74df66a9f752889
DandeOne <dande2@o2.pl>
2018-07-01 23:45:49 +0200

    Basic operations with lambda expressions

---
+++ Calculator Console App/Calculator Console App/Models/Operation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Console_App.Models
+{
+    class Operation
+    {
+        private bool ifDouble;
+        private bool ifInt;
+        private int firstValue;
+        private int secondValue;
+        private double dfirstValue;
+        private double dsecondValue;
+        private int value = 0;
+        private double dvalue =0.0;
+
+        public Operation()
+        {
+            ifDouble = false;
+            ifInt = true;
+
+        }
+        public static int Add(int x, int y) => x + y;
+        public static int Sub(int x, int y) => x - y;
+        public static int Mul(int x, int y) => x * y;
+        public static int Div(int x, int y) => x / y;
+        public static int Mod(int x, int y) => x % y;
+
+        public static double Add(double x, double y) => x + y;
+        public static double Sub(double x, double y) => x - y;
+        public static double Mul(double x, double y) => x * y;
+        public static double Div(double x, double y) => x / y;
+        public static double Mod(double x, double y) => x % y;
+
+        public void Add(int x)
+        {   
+                value += x;  
+        }
+        public void Add(double x)
+        {
+                dvalue += x;
+        }
+        public void Sub(int x)
+        {
+            value -= x;
+        }
+        public void Sub(double x)
+        {
+            dvalue -= x;
+        }
+    }
+}
--- Calculator Console App/Calculator Console App/Program.cs
+++ Calculator Console App/Calculator Console App/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calculator_Console_App.Models;
 
 namespace Calculator_Console_App
 {
@@ -10,6 +11,10 @@ namespace Calculator_Console_App
     {
         static void Main(string[] args)
         {
+            Operation op = new Operation();
+            Console.WriteLine("Result of sum 1 + 2 = " + Operation.Add(1, 2));
+            Console.WriteLine("Result of sum 1,3 + 2,5 = " + Operation.Add(1.3, 2.5));
+            Console.ReadKey();
         }
     }
 }
