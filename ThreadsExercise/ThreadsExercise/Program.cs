2f0b0198f1d01ccb2c484d87b631e1be60bf21de
DandeOne <dande2@o2.pl>
2018-07-10 18:47:27 +0200

    Thread exercise

---
+++ ThreadsExercise/ThreadsExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Main is also thread so Main and Thread y will share time for their operations until they are completed
+             * Thread y = new Thread(Print1);
+            y.Start();
+            for(int i=0;i<1000;i++)
+            {
+                Console.Write(0);
+            }
+            */Console.ReadLine();
+        }
+        static void Print1()
+        {
+            for(int i=0;i<1000;i++)
+            {
+                Console.Write(1);
+            }
+        }
+    }
+}
