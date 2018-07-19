35c80fda4f258da65cdab4ca8b5922619bb7e058
DandeOne <dande2@o2.pl>
2018-07-19 14:04:28 +0200

    Added People Controller with Views - Index and List of People

---
+++ ASPDemoApplication/MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,27 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            people.Add(new PersonModel { FirstName = "Adam", LastName = "Smith", Age = 27 });
+            people.Add(new PersonModel { FirstName = "Eve", LastName = "Wayne", Age = 25 });
+            people.Add(new PersonModel { FirstName = "Cloud", LastName = "Bourne", Age = 26 });
+            return View(people);
+        }
+    }
+}
