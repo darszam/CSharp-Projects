53e00e584100c7883b71619106b6a80f58a40bae
DandeOne <dande2@o2.pl>
2018-07-27 02:03:13 +0200

    Doing Microsoft's tutorial about Core MVC

---
+++ CoreWebAppSolution/DemoCoreWebAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DemoCoreWebAppMVC.Models;
+
+namespace DemoCoreWebAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //int i = 0;
+            //int a = 4 / i;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
