9c999451f8971f4bf814a2d33a366f00d3f5502f
DandeOne <dande2@o2.pl>
2018-07-31 00:37:35 +0200

    Project where I will put many C# features in short, mostly 1-file parts. Today - custom indexator and first part of doing Company Hierarchy program where you will organize employees for practicing OOP

---
+++ Elements_Of_CSharp/ClassyStuff/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassyStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Sentence s = new Sentence();
+            Console.WriteLine(s[0]);
+            s[0] = "New";
+            Console.WriteLine(s[0]);*/
+
+            /* Company Manager Project 
+             * Create an hierarchy of classes - abstract class Employee and subclasses 
+             * HourlyEmployee, SalariedEmployee, Manager and Executive. Every one's pay is calculated differently, 
+             * research a bit about it. After you've established an employee hierarchy, 
+             * create a Company class that allows you to manage the employees. 
+             * You should be able to hire, fire and raise employees.
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Elements_Of_CSharp/ClassyStuff/Sentence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassyStuff
+{
+    class Sentence
+    {
+        private string[] words = "This is normal sentence".Split();
+        public string this [int wordInd]
+        {
+            get { return words[wordInd]; }
+            set { words[wordInd] = value; }
+        }
+    }
+}
