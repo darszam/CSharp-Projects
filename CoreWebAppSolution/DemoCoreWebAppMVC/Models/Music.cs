3d167f46123adfbe241259486de9825214839f81
DandeOne <dande2@o2.pl>
2018-08-09 00:09:20 +0200

    Added database for CRUD

--- CoreWebAppSolution/DemoCoreWebAppMVC/Models/Music.cs
+++ CoreWebAppSolution/DemoCoreWebAppMVC/Models/Music.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace DemoCoreWebAppMVC.Models
 {
     public class Music
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode=true)]
         public DateTime ReleaseTime { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
