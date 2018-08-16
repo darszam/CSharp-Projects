e32333b23dabeba3c4aaf19f9a1178f7057e6792
DandeOne <dande2@o2.pl>
2018-08-17 00:29:37 +0200

    Created SeedData which will initialize and add to server couple of objects of my Music model if database will be empty

--- CoreWebAppSolution/DemoCoreWebAppMVC/Models/Music.cs
+++ CoreWebAppSolution/DemoCoreWebAppMVC/Models/Music.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DemoCoreWebAppMVC.Models
 {
     public class Music
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Time")]
+        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode=true)]
         public DateTime ReleaseTime { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
---
+++ CoreWebAppSolution/DemoCoreWebAppMVC/Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoCoreWebAppMVC.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MusicContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MusicContext>>()))
+            {
+                // Look for any movies.
+                if (context.Music.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Music.AddRange(
+                     new Music
+                     {
+                         Title = "Fine Again",
+                         ReleaseTime = DateTime.Parse("1989-1-11"),
+                         Genre = "Rock",
+                         Price = 7.99M
+                     },
+
+                     new Music
+                     {
+                         Title = "Will Power",
+                         ReleaseTime = DateTime.Parse("1984-3-13"),
+                         Genre = "Rock",
+                         Price = 8.99M
+                     },
+
+                     new Music
+                     {
+                         Title = "Iridium",
+                         ReleaseTime = DateTime.Parse("1986-2-23"),
+                         Genre = "Rock",
+                         Price = 9.99M
+                     },
+
+                   new Music
+                   {
+                       Title = "Paper Moon",
+                       ReleaseTime = DateTime.Parse("1959-4-15"),
+                       Genre = "Rock",
+                       Price = 3.99M
+                   }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
