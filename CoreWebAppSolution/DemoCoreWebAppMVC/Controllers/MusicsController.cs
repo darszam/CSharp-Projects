4873aad2fe5759b9b866b0df6aa963cdc2cc1694
DandeOne <dande2@o2.pl>
2018-08-20 00:05:28 +0200

    Added search field

--- CoreWebAppSolution/DemoCoreWebAppMVC/Controllers/MusicsController.cs
+++ CoreWebAppSolution/DemoCoreWebAppMVC/Controllers/MusicsController.cs
@@ -19,9 +19,16 @@ namespace DemoCoreWebAppMVC.Controllers
         }
 
         // GET: Musics
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            return View(await _context.Music.ToListAsync());
+            var musics = from m in _context.Music
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                musics = musics.Where(s => s.Title.Contains(searchString));
+            }
+            return View(await musics.ToListAsync());
         }
 
         // GET: Musics/Details/5
