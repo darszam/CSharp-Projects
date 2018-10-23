6e1319b2a950aa9f05a795d3b3b3338ad6b39320
DandeOne <dande2@o2.pl>
2018-10-24 00:08:00 +0200

    Some of the migrations

---
+++ MusicApp/MusicApp/Migrations/20181020181039_2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MusicApp.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "MusicAsset",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "MusicAsset");
+        }
+    }
+}
